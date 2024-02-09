60c581a0d2d4ed0e1f37f0ad77ccae29fdfd14f6
ZagloSs <carlosdiezmendez@gmail.com>
2024-02-09 13:02:41 +0100

    main & client

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -1,9 +1,33 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class MainClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Socket socket = new Socket("192.168.128.240", 6565);
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("Introduce el mensaje para el server: ");
+			dos.writeUTF(sc.nextLine());
+
+			System.out.println("Respuesta: " + dis.readUTF());
+
+			sc.close();
+			dos.close();
+			socket.close();
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
